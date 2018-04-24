44e0a3e36c8762769e5d3bc1c663751ff2b3aae3
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-24 02:47:19 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  hash = {}
+  array.collect do |animals|
+    if hash[animals]== nil
+      hash[animals] = 1
+    else
+      hash[animals] += 1
+    end
+  end
+  hash
 end
  
